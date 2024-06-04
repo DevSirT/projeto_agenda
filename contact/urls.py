0e8baf1c3d2470137ad9187552acbe3a2baad5ed
devsirt <devthulio@gmail.com>
2024-06-04 11:24:51 -0300

    Usando UserCreationForm para criar novos usuários no Django

--- contact/urls.py
+++ contact/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
     path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
     path('contact/create/', views.create, name='create'),
 
+    # user (CRUD)
+    path('user/create/', views.register, name='register'),
+
 ]
